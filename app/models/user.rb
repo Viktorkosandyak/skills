1dc362f95edfc8f78a447ded9fdf148f5e9bf5b6
Viktorkosandyak <kosandyak1996@gmail.com>
2018-10-26 17:13:54 +0300

    add botstrap

--- app/models/user.rb
+++ app/models/user.rb
@@ -1,4 +1,8 @@
 class User < ActiveRecord::Base
-  validates :name, presence: true
-  validates :age, presence: true
+  # Include default devise modules. Others available are:
+  # :confirmable, :lockable, :timeoutable and :omniauthable
+  devise :database_authenticatable, :registerable,
+         :recoverable, :rememberable, :trackable, :validatable
+  # validates :age, presence: true
+  validates :email, uniqueness: true
 end
